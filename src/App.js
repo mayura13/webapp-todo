730c0a2a8e8a56bf054209c9f865dd884bf91cdc
Yujin Oh <yu05jin@naver.com>
2026-01-19 15:52:33 +0900

    최초 커밋

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  
+  const handleInputChange = (evt) => setInputValue(evt.target.value);
+  const handleSubmit = (evt) => {
+    if(inputValue.trim() == "") return;
+    const newTodo = {
+      id: Date.now(),
+      text: "abcd",
+      complete: false
+    }, {
+      id: Date.now() +1,
+      text: "1234",
+      complete: true
+    }
+    setTodos([...todos, newTodo]);
+    setInputValue("");
+  }
+
+  const deleteItem = id =>{
+    const filtered = todos.filter(todo=> todo.id != id);
+    setTodos(filtered);
+  }
+
+  const toggleTodo = id => {
+    const updated = todos.map(
+      todo => todo.id === id?{...todo, complete: !todo.complete} :todo
+    );
+    setTodos(updated);
+  };
+
+  return(
+      <div className="App">
+      <h1>Todo App</h1>
+      <div></div>
+      <input value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleSubmit}>확인</button>
+      <ul>
+        {todos.map(todo=> (
+          <li key = {todo.id}>
+            <input type = "checkbox" checked= {todo.complete}
+              onChange={()=> toggleTodo(todo.id)} />
+            <span style= {{textDecoration: todo.complete? 'line-through':'none'}}>
+              {todo.text}
+            </span>
+            <button onClick = {()=>deleteItem(todo.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+    );
+  }
